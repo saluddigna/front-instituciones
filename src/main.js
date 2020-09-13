eb34d545900674e7c596aea9588bd174c0ecb979
HiperEmmanuel <hiperemmanuel@gmail.com>
2020-09-13 16:17:31 -0600

    cuponera

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import('@/assets/css/saluddigna.min.css')
+import print from './directives/print'
 Vue.config.productionTip = false
 
 new Vue({
+  print,
   router,
   vuetify,
   render: h => h(App)
